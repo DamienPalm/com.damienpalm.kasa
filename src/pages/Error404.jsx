565cacf4db6e745f3fd3f1b99584e6a4267e146f
Damien Palm <palm.damien.pro@gmail.com>
2025-01-21 18:18:57 +0100

    creation error page

--- src/pages/Error404.jsx
+++ src/pages/Error404.jsx
@@ -1,9 +1,17 @@
+import { Link } from "react-router";
+import "../styles/Error404.css";
+
 function Error404() {
   return (
-    <div>
-      <h1>Error 404</h1>
-      <p>Page not found</p>
-    </div>
+    <section className="main__error-section">
+      <h1 className="main__error-section__title">404</h1>
+      <p className="main__error-section__text">
+        Oups! La page que vous demandez n&apos;existe pas.
+      </p>
+      <Link to="/" className="main__error-section__link">
+        Retourner sur la page d&apos;accueil
+      </Link>
+    </section>
   );
 }
 
